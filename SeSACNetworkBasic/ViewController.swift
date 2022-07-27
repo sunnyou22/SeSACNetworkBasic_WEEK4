28e2699e3e561577ddd91b1e901e2411a8b66e1a
sunnyou22 <sunnyouyaya22@gmail.com>
2022-07-27 11:42:04 +0900

    Initial Commit

---
+++ SeSACNetworkBasic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACNetworkBasic
+//
+//  Created by 방선우 on 2022/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
